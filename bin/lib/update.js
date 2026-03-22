0b7477ca66beab7b9b4cad7225c7039560f9ed46
vasanth53 <vasanth@peak42.in>
2026-03-22 19:58:41 +0530

    fix: correct package.json path in update.js

--- bin/lib/update.js
+++ bin/lib/update.js
@@ -65,7 +65,7 @@ function fetchUrl(url) {
  */
 function getCurrentVersion() {
   try {
-    const pkg = require("../package.json");
+    const pkg = require("../../package.json");
     return pkg.version || "0.0.0";
   } catch {
     return "0.0.0";
